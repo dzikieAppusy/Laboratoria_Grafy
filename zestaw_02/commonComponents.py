fac8b9d3c02b3103dff30ad1db4b42a622ff5b6b
Maciej <mbrocki@amusys.pl>
2025-05-06 18:49:06 +0200

    Add a comments to the file in zestaw2 to tasks 1,2,3

--- zestaw_02/commonComponents.py
+++ zestaw_02/commonComponents.py
@@ -1,39 +1,47 @@
 from graphicSequence import construct_graph
 from zestaw_01.graph_coder import AdjacencyList
 
-# algorytm z wykładu
-def components_R(nr,v,G,comp):
-    for u in G[v]:
-        if comp[u] == -1:
-            comp[u] = nr
-            components_R(nr,u,G,comp)
+# Rekurencyjna funkcja DFS do oznaczania wierzchołków należących do tej samej spójnej składowej
+# nr – numer bieżącej składowej
+# v – aktualnie odwiedzany wierzchołek
+# G – graf w postaci listy sąsiedztwa
+# comp – lista przypisująca wierzchołkom numer składowej (-1 oznacza nieodwiedzony)
+def components_R(nr, v, G, comp):
+    for u in G[v]:  # Przechodzimy przez sąsiadów v
+        if comp[u] == -1:  # Jeśli wierzchołek nie został jeszcze odwiedzony
+            comp[u] = nr  # Przypisujemy mu numer bieżącej składowej
+            components_R(nr, u, G, comp)  # Rekurencyjnie odwiedzamy sąsiadów
 
-# Jako parametr G jest podawany graf w postaci listy sąsiedztwa
+# G – graf jako lista sąsiedztwa (np. G[0] = [1,2] oznacza, że wierzchołek 0 ma krawędzie do 1 i 2)
 def components(G):
-    nr = 0
-    n = len(G)
-    comp = [-1]*n
+    nr = 0  # Numerowanie składowych spójnych, zaczynamy od 0 i zwiększamy przy każdej nowej
+    n = len(G)  # Liczba wierzchołków
+    comp = [-1] * n  # Tworzymy listę oznaczającą do której składowej należy dany wierzchołek
 
-    for v in range(n):
-        if comp[v] == -1:
-            nr += 1
+    for v in range(n):  # Iterujemy po wszystkich wierzchołkach
+        if comp[v] == -1:  # Jeśli wierzchołek nie został jeszcze przypisany do składowej
+            nr += 1  # Zaczynamy nową składową
             comp[v] = nr
-            components_R(nr,v,G,comp)
+            components_R(nr, v, G, comp)  # Uruchamiamy DFS dla tej składowej
 
-    return comp
+    return comp  # Zwracamy listę przypisań wierzchołków do składowych
 
-# Funkcja dzieląca na grupy dla lepszego obrazowania wspólnych składowych
+# Funkcja grupuje wierzchołki według ich przypisanej składowej
+# comp – lista zawierająca numer składowej dla każdego wierzchołka
 def group_components(comp):
-    listComp = {}
-    for i, u in enumerate(comp):
+    listComp = {}  # Słownik: klucz to numer składowej, wartość to lista wierzchołków
+    for i, u in enumerate(comp):  # Iterujemy po liście przypisań
         if u not in listComp:
-            listComp[u] = []
-        listComp[u].append(i)
-    return listComp
+            listComp[u] = []  # Tworzymy nową listę jeśli to pierwszy wierzchołek w tej składowej
+        listComp[u].append(i)  # Dodajemy wierzchołek do odpowiedniej składowej
+    return listComp  # Zwracamy słownik grup wierzchołków
 
 #Tests
-A = [1, 3, 2, 3, 2, 4, 1]
-adj_list_A = construct_graph(A)
-g = components(adj_list_A)
-
-print("Wspólne składowe: {}".format(group_components(g)))
+if __name__ == '__main__':
+    A = [1, 3, 2, 3, 2, 4, 1]
+    adj_list_A = construct_graph(A)
+    AdjList = AdjacencyList(len(A))
+    AdjList.list = adj_list_A
+    AdjList.visualize()
+    g = components(adj_list_A)
+    print("Wspólne składowe: {}".format(group_components(g)))
--- zestaw_02/graphicSequence.py
+++ zestaw_02/graphicSequence.py
@@ -1,94 +1,115 @@
 from zestaw_01.graph_coder import AdjacencyList
 import random
 
+# Funkcja pomocnicza: sprawdza, czy wszystkie elementy listy A są równe zero
 def check_zero_elements(A):
     return all(a == 0 for a in A)
 
 # Zadanie 1
-# Funkcja sprawdzająca czy dana sekwencja liczb jest ciągiem graficznym
+# Funkcja sprawdzająca, czy dana sekwencja liczb jest ciągiem graficznym (algorytm Havel-Hakimi)
 def graphic_sequence(A):
-    A = A[:]
-    A.sort(reverse=True)
+    A = A[:]  # Tworzymy kopię listy, żeby nie modyfikować oryginału
+    A.sort(reverse=True)  # Sortujemy malejąco
 
+    # Dopóki lista nie jest pusta i największy element > 0
     while A and A[0] > 0:
         if check_zero_elements(A):
-            return True
-        if A[0] >= len(A):
-            return False
+            return True  # Wszystkie stopnie są 0, czyli to ciąg graficzny
 
-        d = A[0]
-        A = A[1:]
+        if A[0] >= len(A):
+            return False  # Nie można połączyć tylu wierzchołków - błąd
+
+        d = A[0]  # Największy stopień
+        A = A[1:]  # Usuwamy ten element
 
         if len(A) < d:
-            return False
+            return False  # Nie ma wystarczającej liczby wierzchołków do połączenia
 
+        # Odejmuje 1 od kolejnych d elementów (symulując połączenia)
         for i in range(d):
             A[i] -= 1
             if A[i] < 0:
-                return False
+                return False  # Stopień nie może być ujemny
 
-        A.sort(reverse=True)
+        A.sort(reverse=True)  # Sortujemy ponownie
 
-    return True
+    return True  # Jeśli pętla się skończy, to ciąg jest graficzny
 
-# Funkcja służąca do stworzenia grafu z podanej sekwencji licz naturalnych
+# Funkcja tworzy graf (listę sąsiedztwa) z danej sekwencji stopni (jeśli jest graficzna)
 def construct_graph(A):
-    A = A[:]
+    A = A[:]  # Tworzymy kopię
+    # Sortujemy pary (indeks, stopień) malejąco po stopniu
     A = sorted(enumerate(A), key=lambda x: x[1], reverse=True)
     n = len(A)
 
+    # Rozdzielamy indeksy i stopnie
     degrees = [deg for _, deg in A]
     nodes = [idx for idx, _ in A]
 
+    # Tworzymy pustą listę sąsiedztwa
     adj_list = {i: [] for i in nodes}
 
+    # Dopóki są jeszcze stopnie do przypisania
     while degrees and degrees[0] > 0:
         if degrees[0] >= len(degrees):
-            return None
+            return None  # Nie można połączyć tylu wierzchołków
 
-        d = degrees[0]
-        node = nodes[0]
+        d = degrees[0]  # Największy stopień
+        node = nodes[0]  # Wierzchołek o największym stopniu
 
+        # Łączymy ten wierzchołek z kolejnymi d wierzchołkami
         for i in range(1, d + 1):
             if degrees[i] == 0:
-                return None
+                return None  # Nie można już użyć wierzchołka bez stopnia
 
+            # Dodajemy krawędź do obu list sąsiedztwa (graf nieskierowany)
             adj_list[node].append(nodes[i])
             adj_list[nodes[i]].append(node)
-            degrees[i] -= 1  # Reduce degree
+            degrees[i] -= 1  # Zmniejszamy stopień
 
-        degrees[0] = 0
-        nodes.pop(0)
+        degrees[0] = 0  # Aktualny wierzchołek już został przetworzony
+        nodes.pop(0)    # Usuwamy go z listy
         degrees.pop(0)
 
+        # Sortujemy pozostałe wierzchołki po aktualnych stopniach malejąco
         combined = sorted(zip(nodes, degrees), key=lambda x: x[1], reverse=True)
         if combined:
             nodes, degrees = zip(*combined)
             nodes, degrees = list(nodes), list(degrees)
 
+    # Zwracamy listę sąsiedztwa, jeśli wszystkie stopnie zostały wykorzystane
     return adj_list if all(d == 0 for d in degrees) else None
 
 # Zadanie 2
-def randomize_graph(adj_list, iterations = 10):
-    edges = list(set((min(a,b), max(a,b)) for a in adj_list for b in adj_list[a]))
+# Funkcja losowo modyfikuje graf (bez zmiany stopni wierzchołków) poprzez przetasowywanie krawędzi
+import random
 
+def randomize_graph(adj_list, iterations=10):
+    # Tworzymy listę unikalnych krawędzi w formacie (mniejszy_wierzchołek, większy_wierzchołek)
+    edges = list(set((min(a, b), max(a, b)) for a in adj_list for b in adj_list[a]))
+
+    # Wykonujemy podaną liczbę iteracji przetasowywania
     for _ in range(iterations):
         if len(edges) < 2:
-            break
-        (a,b), (c,d) = random.sample(edges, 2)
+            break  # Potrzebujemy przynajmniej dwóch krawędzi do zamiany
 
-        if a==d or b==c in edges or (b,c) in edges:
-            continue
+        # Losowo wybieramy dwie różne krawędzie
+        (a, b), (c, d) = random.sample(edges, 2)
 
-        # Usuwanie starych krawędzi
-        edges.remove((a,b))
-        edges.remove((c,d))
+        # Sprawdzamy warunki bezpieczeństwa przed zamianą
+        # Unikamy tworzenia pętli (np. a==d), i sprawdzamy czy nowe krawędzie już nie istnieją
+        if a == d or b == c or (a, d) in edges or (b, c) in edges or (d, a) in edges or (c, b) in edges:
+            continue  # Jeśli nowe krawędzie byłyby niepoprawne, pomijamy iterację
+
+        # Usuwamy stare krawędzie z listy i listy sąsiedztwa
+        edges.remove((a, b))
+        edges.remove((c, d))
         adj_list[a].remove(b)
         adj_list[b].remove(a)
         adj_list[c].remove(d)
         adj_list[d].remove(c)
 
-        # dodawanie nowych krawędzi
+        # Dodajemy nowe krawędzie do listy i listy sąsiedztwa
         edges.append((a, d))
         edges.append((b, c))
         adj_list[a].append(d)
@@ -96,23 +117,25 @@ def randomize_graph(adj_list, iterations = 10):
         adj_list[b].append(c)
         adj_list[c].append(b)
 
+    # Zwracamy zmodyfikowaną listę sąsiedztwa
     return adj_list
 
-# Test cases
-A = [1, 3, 2, 3, 2, 4, 1]
-B = [1, 2, 4]
+if __name__ == '__main__':
+    # Test cases
+    A = [1, 3, 2, 3, 2, 4, 1]
+    B = [1, 2, 4]
 
-print(graphic_sequence(A))
-print(graphic_sequence(B))
+    print(graphic_sequence(A))
+    print(graphic_sequence(B))
 
-adj_list_A = construct_graph(A)
-print(adj_list_A)
+    adj_list_A = construct_graph(A)
+    print(adj_list_A)
 
-AdjList = AdjacencyList(len(A))
-AdjList.list = adj_list_A
-AdjList.display()
-AdjList.visualize()
+    AdjList = AdjacencyList(len(A))
+    AdjList.list = adj_list_A
+    AdjList.display()
+    AdjList.visualize()
 
-# Testy randomizacji grafu
-AdjList.list=randomize_graph(AdjList.list)
-AdjList.visualize()
+    # Testy randomizacji grafu
+    AdjList.list=randomize_graph(AdjList.list)
+    AdjList.visualize()
